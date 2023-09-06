1e8067b52e89f97fe6efa5efbedd50e2dafdbecd
Melly19888 <139958288+Melly19888@users.noreply.github.com>
2023-09-06 11:05:28 +0200

    Add files via upload
    Spiel komplett fertig

---
+++ Pacman/src/main/java/pacman/Pacman.java
@@ -0,0 +1,39 @@
+package pacman;
+
+
+
+
+
+
+
+
+import javax.swing.JFrame;
+
+public class Pacman extends JFrame{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+
+    public Pacman() {
+        add(new Model());
+    }
+
+
+
+    public static void main(String[] args) {
+        Pacman pac = new Pacman();
+        pac.setVisible(true);
+        pac.setTitle("Pacman");
+        pac.setSize(400,430);
+        pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pac.setLocationRelativeTo(null);
+
+
+
+    }
+
+}
+
